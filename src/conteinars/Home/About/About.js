19bc8288469a7a7db982ec8d92f68228b342c934
urmiljoshi <urmiljoshi16@gmail.com>
2022-08-21 11:29:43 +0530

    18_8_22

--- src/conteinars/Home/About/About.js
+++ src/conteinars/Home/About/About.js
@@ -1,30 +1,104 @@
 import React from 'react';
 
 function About(props) {
-    return (
+  return (
+    <div>
+      <section id="about" class="about">
         <div>
-            <section id="about" class="about">
-    <div class="container">
 
-      <div class="row">
+          <div class="row">
 
 
-        <div
-          class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center px-lg-5 abouttop">
-          <h3>Fusce nec risus at enim congue bibendum quis at augue. </h3>
-          <p>Proin tincidunt blandit fermentum. Ut gravida arcu non mi dapibus ullamcorper. Curabitur mollis, turpis eu
+            <div
+              class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center px-lg-5 abouttop">
+            
+              <img src="assets/img/testimonials/testimonials-1.jpg" className="testimonial-img" alt />
+              <p>Proin tincidunt blandit fermentum. Ut gravida arcu non mi dapibus ullamcorper. Curabitur mollis, turpis eu
             pellentesque finibus, nisi ex mattis quam, mollis aliquet mi massa non nunc. Pellentesque id felis elit.
             Pellentesque blandit sem a nisi dictum, in pretium ante tincidunt.</p>
-          <p>Maecenas lobortis, nunc eu porttitor posuere, neque lectus rutrum leo, sit amet rutrum orci eros aliquam
+              <p>Maecenas lobortis, nunc eu porttitor posuere, neque lectus rutrum leo, sit amet rutrum orci eros aliquam
             mauris. Aliquam erat volutpat. Aenean eget dui ac lectus rutrum aliquam pulvinar ut massa. Duis sagittis
-            rutrum neque, quis tincidunt arcu pretium ac. Suspendisse sem </p>
+            rutrum neque, quis tincidunt arcu pretium ac. Suspendisse sem </p> 
+
+
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+        <section id="gallery" className="gallery">
+      <div className="container">
+        <div className="section-title">
+          <h2>Hospital</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+            consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
+            in iste officiis commodi quidem hic quas.</p>
         </div>
       </div>
-
-    </div>
-  </section>
+      <div className="container-fluid">
+        <div className="row no-gutters">
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="assets/img/gallery/gallery-9.png" className="galelry-lightbox">
+                <img src="assets/img/gallery/gallery-9.png" alt className="img-fluid" />
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="assets/img/gallery/gallery-10.png" className="galelry-lightbox">
+                <img src="assets/img/gallery/gallery-10.png" alt className="img-fluid" />
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="assets/img/gallery/gallery-11.png" className="galelry-lightbox">
+                <img src="assets/img/gallery/gallery-11.png" alt className="img-fluid" />
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="assets/img/gallery/gallery-12.png" className="galelry-lightbox">
+                <img src="assets/img/gallery/gallery-12.png" alt className="img-fluid" />
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="assets/img/gallery/gallery-13.png" className="galelry-lightbox">
+                <img src="assets/img/gallery/gallery-13.png" alt className="img-fluid" />
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="assets/img/gallery/gallery-14.png" className="galelry-lightbox">
+                <img src="assets/img/gallery/gallery-14.png" alt className="img-fluid" />
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="assets/img/gallery/gallery-7.jpg" className="galelry-lightbox">
+                <img src="assets/img/gallery/gallery-7.jpg" alt className="img-fluid" />
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4">
+            <div className="gallery-item">
+              <a href="assets/img/gallery/gallery-8.jpg" className="galelry-lightbox">
+                <img src="assets/img/gallery/gallery-8.jpg" alt className="img-fluid" />
+              </a>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </section>
+    </div>
+  );
 }
 
 export default About;
