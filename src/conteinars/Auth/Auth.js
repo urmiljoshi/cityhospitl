01878ce033c93b0c1ef48cfb117c237dbf33631d
urmiljoshi <urmiljoshi16@gmail.com>
2022-09-12 11:28:45 +0530

    12_9_22_hendaing

--- src/conteinars/Auth/Auth.js
+++ src/conteinars/Auth/Auth.js
@@ -1,25 +1,47 @@
 import React, { useRef, useState } from 'react';
+import * as yup from 'yup';
 
 function Auth(props) {
     const [userType, setuserType] = useState('Login/sigup');
     const [reset, setreset] = useState(false)
 
-    const  nameRef = useRef();
-    const  emailRef = useRef();
-    const  passRef  =useRef();
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passRef = useRef();
 
-        function handel() {
-            passRef.current.focus();
-            passRef.current.style.border="2px solid yellow"
-            console.log(emailRef.current.value);
-            console.log(passRef.current.value);
-            
+    function handel() {
+        passRef.current.focus();
+        passRef.current.style.border = "2px solid yellow"
+        console.log(emailRef.current.value);
+        console.log(passRef.current.value);
+
+    }
+    function handelon() {
+        nameRef.current.focus();
+        nameRef.current.style.border = '2px solid blue'
+    }
+
+    let authschema;
+    let schema = yup.object().shape(authschema);
+    if (userType === 'singup' && 'reset') {
+        authschema = {
+            name: yup.string().required("please Enter your name"),
+            email: yup.string().required("please Enter valid email").email("please Enter email"),
+            pssword: yup.string().required("Enter your pssword").min(8)
         }
 
-        function handelon() {
-            nameRef.current.focus();
-            nameRef.current.style.border='2px solid blue'
+    } else if (userType === 'login') {
+        authschema = {
+            email: yup.string().required("please Enter valid email").email("please Enter email"),
+            pssword: yup.string().required("Enter your pssword").min(8),
         }
+    } else if (userType === 'true') {
+        authschema = {
+            email: yup.string().required("please Enter valid email").email("please Enter email"),
+        }
+    }
+
+
 
 
     return (
@@ -70,7 +92,7 @@ function Auth(props) {
                             :
                             <div className="row">
                                 <div className="col-md-4 form-group mt-3 mt-md-0">
-                                    <input ref={passRef}  type="tel" className="form-control" name="pssword" id="pssword" placeholder="Your pssword" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                    <input ref={passRef} type="tel" className="form-control" name="pssword" id="pssword" placeholder="Your pssword" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                                     <div className="validate" />
                                 </div>
                             </div>
@@ -79,19 +101,19 @@ function Auth(props) {
 
                     {
                         reset === true ?
-                        <div className="text-center"><button type="submit">submit</button></div>
-                        :
-                        userType === 'Login' ?
-                            <div className="text-center"><button type="submit" onClick={() => handel()}>Login</button></div>
+                            <div className="text-center"><button type="submit">submit</button></div>
                             :
-                            <div className="text-center"><button type="submit" onClick={() => handelon()} >sigup</button></div>
+                            userType === 'Login' ?
+                                <div className="text-center"><button type="submit" onClick={() => handel()}>Login</button></div>
+                                :
+                                <div className="text-center"><button type="submit" onClick={() => handelon()} >sigup</button></div>
                     }
 
                     {
                         userType === 'Login' ?
-                            <div>Create a new Account1 <button onClick={() => {setreset(false); setuserType('Singup')}}>Signup</button></div>
+                            <div>Create a new Account1 <button onClick={() => { setreset(false); setuserType('Singup') }}>Signup</button></div>
                             :
-                            <div>Already have Account1 <button onClick={() => {setreset(false); setuserType('Login')}}>Login</button></div>
+                            <div>Already have Account1 <button onClick={() => { setreset(false); setuserType('Login') }}>Login</button></div>
 
                     }
                     <span>Forget password <button onClick={() => setreset(true)}>Click Here</button></span>
